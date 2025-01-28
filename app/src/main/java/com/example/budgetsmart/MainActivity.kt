33dcd0bef9ef71801d3f0b303c27acf50de35c09
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-01-28 20:36:17 +0200

    Removing navigation bar handling

--- app/src/main/java/com/example/budgetsmart/MainActivity.kt
+++ app/src/main/java/com/example/budgetsmart/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.budgetsmart
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import com.example.budgetsmart.Fragments.HomeFragment
-import com.example.budgetsmart.Fragments.TransactionsFragment
+import com.example.budgetsmart.fragments.HomeFragment
+import com.example.budgetsmart.fragments.ProfileSettingsFragment
+import com.example.budgetsmart.fragments.SettingsFragment
+import com.example.budgetsmart.fragments.TransactionsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
     class MainActivity : AppCompatActivity() {
@@ -17,9 +18,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
 
         val homeFragment = HomeFragment()
         val transactionsFragment = TransactionsFragment()
+        val settingsFragment = SettingsFragment()
 
         findViews()
         replaceFragment(homeFragment)
@@ -28,14 +31,28 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
             when (it.itemId) {
                 R.id.Home -> replaceFragment(homeFragment)
                 R.id.transactions -> replaceFragment(transactionsFragment)
-                R.id.Settings -> replaceFragment(homeFragment)
+                R.id.Settings -> replaceFragment(settingsFragment)
                 else -> replaceFragment(homeFragment)
             }
             true
         }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+            when (currentFragment) {
+                is ProfileSettingsFragment -> hideBottomNavigation()
+                else -> showBottomNavigation()
+            }
+        }
     }
 
+    private fun showBottomNavigation() {
+        bottom_navigation.visibility = View.VISIBLE
+    }
 
+    private fun hideBottomNavigation() {
+        bottom_navigation.visibility = View.GONE
+    }
 
     private fun findViews() {
         bottom_navigation = findViewById(R.id.bottom_navigation)

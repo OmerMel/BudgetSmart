0f762aa357ef16fc495efbcfdce1949a5f097065
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-01-28 20:37:07 +0200

    AddExpenseFragment init

---
+++ app/src/main/java/com/example/budgetsmart/fragments/AddExpenseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.budgetsmart.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.budgetsmart.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class AddExpenseFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_expense, container, false)
+    }
+}

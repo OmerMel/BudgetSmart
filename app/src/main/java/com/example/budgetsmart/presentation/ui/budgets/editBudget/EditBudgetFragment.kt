190f01498d1711cf319937e98e83fa41b746ca12
Omer Meler <omermele52@gmail.com>
2025-02-04 23:00:05 +0200

    EditBudgetViewModel.kt deleted

--- app/src/main/java/com/example/budgetsmart/presentation/ui/budgets/editBudget/EditBudgetFragment.kt
+++ app/src/main/java/com/example/budgetsmart/presentation/ui/budgets/editBudget/EditBudgetFragment.kt
@@ -1,6 +1,5 @@
 package com.example.budgetsmart.presentation.ui.budgets.editBudget
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,18 +8,6 @@ import android.view.ViewGroup
 import com.example.budgetsmart.R
 
 class EditBudgetFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = EditBudgetFragment()
-    }
-
-    private val viewModel: EditBudgetViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/budgetsmart/presentation/ui/budgets/editBudget/EditBudgetViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.budgetsmart.presentation.ui.budgets.editBudget
-
-import androidx.lifecycle.ViewModel
-
-class EditBudgetViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}

ce70f1e554c4a4c1b82b8a277c6ced2526dda400
Omer Meler <omermele52@gmail.com>
2025-02-08 23:30:48 +0200

    BudgetCategoryType init

---
+++ app/src/main/java/com/example/budgetsmart/domain/model/enums/BudgetCategoryType.kt
@@ -0,0 +1,11 @@
+package com.example.budgetsmart.domain.model.enums
+
+enum class BudgetCategoryType(val displayName: String) {
+    HOUSING("Housing"),
+    FOOD("Food & Dining"),
+    TRANSPORTATION("Transportation"),
+    ENTERTAINMENT("Entertainment"),
+    UTILITIES("Utilities"),
+    SHOPPING("Shopping"),
+    OTHER("Other")
+}
